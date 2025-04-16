948ee9898974732108587b6e8e2cdfccbefd76d5
christina-baldwin <christina.baldwin13@yahoo.com>
2025-04-16 10:32:05 +0200

    changing order of the elemnts in the project component

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,12 +2,12 @@ const Project = (props) => {
   return (
     <div key={props.title} className="project">
       <h3>{props.title}</h3>
-      <p>{props.description}</p>
       <ul>
         {props.skills.map((skill, index) => (
           <li key={index}>{skill}</li>
         ))}
       </ul>
+      <p>{props.description}</p>
       <p>#{props.tag}-project</p>
       <a href={props.link}>To Project Demo</a>
       <a href={props.github}>View Code</a>
