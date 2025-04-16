a576722a7403dcb2ace7396c85ad72c6873b1d7b
christina-baldwin <christina.baldwin13@yahoo.com>
2025-04-16 10:55:21 +0200

    adding the skills section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Hero from "./components/Hero";
 import Tech from "./components/Tech";
 import Projects from "./components/Projects";
+import Skills from "./components/Skills";
 
 export const App = () => {
   return (
@@ -11,6 +12,7 @@ export const App = () => {
       ></Hero>
       <Tech />
       <Projects />
+      <Skills />
     </>
   );
 };
---
+++ src/components/Skills.jsx
@@ -0,0 +1,21 @@
+import skillsData from "../data/skills.json";
+
+const Skills = () => {
+  return (
+    <div className="skills">
+      <h2>Skills</h2>
+      {skillsData.skills.map((skill) => (
+        <div key={skill.name}>
+          <h3>{skill.name}</h3>
+          <ul>
+            {skill.skills.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Skills;
