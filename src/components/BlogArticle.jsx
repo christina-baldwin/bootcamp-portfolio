355beab68f284ff96051fdc0aae24da8b9fbad16
christina-baldwin <christina.baldwin13@yahoo.com>
2025-04-16 15:41:00 +0200

    styling the blog article setup

--- src/components/BlogArticle.jsx
+++ src/components/BlogArticle.jsx
@@ -2,11 +2,13 @@ const BlogArticle = (props) => {
   return (
     <div key={props.title} className="article">
       <img src={props.img} />
-      <p>{props.date}</p>
-      <h3>{props.title}</h3>
-      <p>{props.desc}</p>
-      <p>#{props.type}-article</p>
-      <a href={props.link}>Read article</a>
+      <div className="article-text">
+        <p>{props.date}</p>
+        <h3>{props.title}</h3>
+        <p>{props.desc}</p>
+        <p>#{props.type}-article</p>
+        <a href={props.link}>Read article</a>
+      </div>
     </div>
   );
 };
