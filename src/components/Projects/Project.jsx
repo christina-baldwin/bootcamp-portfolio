94eb5398426bc5e10ade51a55346305490f2951b
christina-baldwin <christina.baldwin13@yahoo.com>
2025-04-17 10:56:06 +0200

    styling the project cards

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,16 +1,26 @@
+import "./Project.css";
+
 const Project = (props) => {
   return (
     <div key={props.title} className="project m-b-m">
       <h3>{props.title}</h3>
-      <ul className="m-b-s">
+      <ul className="skill-tags m-b-s">
         {props.skills.map((skill, index) => (
-          <li key={index}>{skill}</li>
+          <li className="skill-tag" key={index}>
+            {skill}
+          </li>
         ))}
       </ul>
       <p className="m-b-s">{props.description}</p>
-      <p className="m-b-s">#{props.tag}-project</p>
-      <a href={props.link}>To Project Demo</a>
-      <a href={props.github}>View Code</a>
+      <p className="project-tag m-b-s">#{props.tag}-project</p>
+      <div className="project-links">
+        <a className="project-link demo-link" href={props.link}>
+          <ion-icon name="globe-outline"></ion-icon>To Project Demo
+        </a>
+        <a className="project-link git-link" href={props.github}>
+          <ion-icon name="logo-github"></ion-icon>View Code
+        </a>
+      </div>
     </div>
   );
 };
