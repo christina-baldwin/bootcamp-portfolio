dc5375a044bdfb3247855c28923680f82fb3fef1
christina-baldwin <christina.baldwin13@yahoo.com>
2025-04-23 15:34:29 +0200

    adding a wrapper to hold my app content

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,31 @@ import Skills from "./components/Skills/Skills.jsx";
 import Blog from "./components/Blog/Blog.jsx";
 import Contact from "./components/Contact/Contact.jsx";
 import GlobalStyle from "./GlobalStyles.jsx";
+import styled from "styled-components";
+
+const AppWrapper = styled.div`
+  max-width: 1500px;
+  margin: 0 auto;
+  padding: 0 1rem; // some side padding for breathing room
+`;
 
 export const App = () => {
   return (
     <>
       <GlobalStyle />
-      <Hero
-        logo="/logo192.png"
-        img="/hero-img.png"
-        name="Christina Baldwin"
-        title="A Frontend Developer with a Background in Sustainability"
-      />
-      <Tech />
-      <Projects />
-      <Skills />
-      <Blog />
-      <Contact />
+      <AppWrapper>
+        <Hero
+          logo="/logo192.png"
+          img="/hero-img.png"
+          name="Christina Baldwin"
+          title="A Frontend Developer with a Background in Sustainability"
+        />
+        <Tech />
+        <Projects />
+        <Skills />
+        <Blog />
+        <Contact />
+      </AppWrapper>
     </>
   );
 };
