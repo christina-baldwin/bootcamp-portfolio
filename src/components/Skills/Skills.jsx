099aec22e17fd1e91eab462a0315e3a867ec8947
christina-baldwin <christina.baldwin13@yahoo.com>
2025-04-23 11:30:11 +0200

    moving skills css to styled components

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,23 +1,48 @@
 import skillsData from "../../data/skills.json";
-import "./Skills.css";
+import styled from "styled-components";
+
+const SkillsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const SkillsTable = styled.div`
+  display: flex;
+  gap: 2rem;
+  margin: 0 auto;
+`;
+
+const SkillsList = styled.ul`
+  list-style: none;
+`;
+
+const SkillName = styled.li`
+  list-style: none;
+  background-color: #4ecdc4;
+  border-radius: 5px;
+  padding: 0.5rem;
+`;
 
 const Skills = () => {
   return (
-    <div className="skills m-b-l p-l bg-colour-1">
+    <SkillsContainer className="m-b-l p-l bg-colour-1">
       <h2 className="text-center">Skills</h2>
-      <div className="skills-table">
+      <SkillsTable>
         {skillsData.skills.map((skill) => (
           <div key={skill.name}>
-            <h4 className="skill-name text-center p-s">{skill.name}</h4>
-            <ul>
+            <SkillName className="skill-name text-center p-s">
+              {skill.name}
+            </SkillName>
+            <SkillsList>
               {skill.skills.map((item, index) => (
                 <li key={index}>{item}</li>
               ))}
-            </ul>
+            </SkillsList>
           </div>
         ))}
-      </div>
-    </div>
+      </SkillsTable>
+    </SkillsContainer>
   );
 };
 
